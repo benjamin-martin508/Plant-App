6b316921c112419dd13a3c8d6c8891a68da36132
Benjamin Martin <benjaminmartin@Benjamins-MacBook-Pro.local>
2019-06-19 20:32:20 -0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Plant App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PlantPage(title: 'Plant App'),
+    );
+  }
+}
+
+class PlantPage extends StatefulWidget {
+  PlantPage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _PlantPageState createState() => _PlantPageState();
+}
+
+class _PlantPageState extends State<PlantPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
