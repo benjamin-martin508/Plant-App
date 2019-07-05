383dfc9c38c45ffbc54f69396d9bfd93da89a4a5
benjamin-martin508 <benjamin.martin@smschool.us>
2019-07-05 16:16:16 -0700

    beast

--- lib/main.dart
+++ lib/main.dart
@@ -15,37 +15,64 @@ class MyApp extends StatelessWidget {
         canvasColor: Colors.white,
 //
       ),
-      home: PlantPage(title: 'PlantMonitor'),
+      home: PlantPage(plantname: null),
     );
   }
 }
 
 class PlantPage extends StatefulWidget {
-  PlantPage({Key key, this.title}) : super(key: key);
-  final String title;
+  PlantPage({Key key, this.plantname}) : super(key: key);
+  String plantname;
   @override
   _PlantPageState createState() => _PlantPageState();
 }
 
 class _PlantPageState extends State<PlantPage> {
   var plants = ['betty', 'sallly', 'marvin'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title, style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blueGrey[600],
-      ),
-      drawer: Drawer(
-          child: ListView.separated(
-              itemCount: plants.length,
-              separatorBuilder: (context, position) => Divider(),
-              itemBuilder: (context, position) {
-                return ListTile(
-                    title: Text(plants[position]),
-                    onTap: () => Navigator.pop(context));
-              })),
-      body: Center(child:Container(width:MediaQuery.of(context).size.width*.4,decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/tree_planting.v2.png',),fit: BoxFit.scaleDown)),)),
-    );
+        appBar: AppBar(
+          title: widget.plantname == null
+              ? Text('Plant Monitor', style: TextStyle(color: Colors.white))
+              : Text(widget.plantname, style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey[600],
+        ),
+        drawer: Drawer(
+            child: ListView.separated(
+                itemCount: plants.length,
+                separatorBuilder: (context, position) => Divider(),
+                itemBuilder: (context, position) {
+                  return ListTile(
+                      title: Text(plants[position]),
+                      onTap: () {
+                        setState(() {
+                          widget.plantname = plants[position];
+                        });
+                        Navigator.pop(context);
+                      });
+                })),
+        body: widget.plantname != null ? buildnamedplant() : buildnoplant());
+  }
+
+  buildnoplant() {
+    return Center(
+        child: Container(
+      width: MediaQuery.of(context).size.width * .4,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                'assets/tree_planting.v2.png',
+              ),
+              fit: BoxFit.scaleDown)),
+    ));
+  }
+
+  buildnamedplant() {
+    return Center(
+        child: Container(
+            width: MediaQuery.of(context).size.width * .4,
+            child: Text(widget.plantname)));
   }
 }
