c46de6f2f2a6d2853b319618acc6509b00616f6d
benjamin-martin508 <benjamin.martin@smschool.us>
2019-08-03 13:17:19 -0700

    beast

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class PlantPage extends StatefulWidget {
 
 class _PlantPageState extends State<PlantPage> {
   var plants = ['betty', 'sallly', 'marvin'];
+  var isedit = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +39,7 @@ class _PlantPageState extends State<PlantPage> {
               ? Text('Plant Monitor', style: TextStyle(color: Colors.white))
               : Text(widget.plantname, style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blueGrey[600],
+          actions: <Widget>[buildactions()],
         ),
         drawer: Drawer(
             child: ListView.separated(
@@ -53,7 +55,25 @@ class _PlantPageState extends State<PlantPage> {
                         Navigator.pop(context);
                       });
                 })),
-        body: widget.plantname != null ? buildnamedplant() : buildnoplant());
+        body: widget.plantname != null
+            ? isedit ? buildeditplant() : buildnamedplant()
+            : buildnoplant());
+  }
+
+  buildactions() {
+    if (isedit) {
+      return IconButton(
+          icon: Icon(Icons.done, color: Colors.white),
+          onPressed: () => setState(() {
+                isedit = false;
+              }));
+    } else {
+      return IconButton(
+          icon: Icon(Icons.edit, color: Colors.white),
+          onPressed: () => setState(() {
+                isedit = true;
+              }));
+    }
   }
 
   buildnoplant() {
@@ -71,14 +91,35 @@ class _PlantPageState extends State<PlantPage> {
 
   buildnamedplant() {
     return Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      CircleAvatar(radius: 70, backgroundColor: Colors.amber,backgroundImage:AssetImage(
-            'assets/tree_planting.v2.png'),),
-      Text(widget.plantname),
-      Text('Pothos "Apepremnum Aureum"')
-        ]
-    )
-    );
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          CircleAvatar(
+            radius: 70,
+            backgroundColor: Colors.white,
+            backgroundImage: AssetImage('assets/tree_planting.v2.png'),
+          ),
+          Text(widget.plantname),
+          Text("hjkbkb")
+        ]));
+  }
+
+  buildeditplant() {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          CircleAvatar(
+            radius: 70,
+            backgroundColor: Colors.white,
+            backgroundImage: AssetImage('assets/tree_planting.v2.png'),
+          ),
+          TextField(
+              decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: (widget.plantname),
+          ),onSubmitted: (v) => setState((){widget.plantname= v;}),),
+          Text("edit")
+        ]));
   }
 }
